96fc6b123aa6a2fb0c280389320e6ca03318a932
Ziana Mega Devi <zianamegadevi@gmail.com>
2022-09-05 20:18:20 +0700

    [Challenge] add pass-code

--- src/features/shared/components/PinPage.js
+++ src/features/shared/components/PinPage.js
@@ -1,18 +1,64 @@
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { useTheme } from "../context/ThemeContext"
 import MainContainer from "./MainContainer";
 import FormButton from "./FormButton";
-import {ROUTE} from '../constants';
 
 const PinPage = () => {
     const theme = useTheme();
     const navigation = useNavigation();
     const styles= styling(theme)
-    const [pin, setPin] = useState('');
+    const [pin, setPin] = useState(['', '', '', '']);
     const route = useRoute();
     const [pinParam, setPinParam] = useState({});
+
+    var arr = [];
+    do {
+        let num = Math.floor(Math.random() * 10);
+        arr.push(num);
+        arr = arr.filter((item, index) => {
+          return arr.indexOf(item) === index
+        });
+    } while (arr.length < 10);
+
+    const renderItem = ({item}) => {
+        return (
+            <>
+                <TouchableOpacity key={item} onPress={() => onPressNumber(item)}>
+                    <View style={[styles.inputView, styles.inputOval]}>
+                        <Text style={styles.textNumber}>{item}</Text>
+                    </View>
+                </TouchableOpacity>
+            </>
+        )
+    }
+
+    const onPressNumber = (num) => {
+        let tempCode = [...pin];
+        for (var i = 0; i < tempCode.length; i++) {
+            if(tempCode[i] === '') {
+                tempCode[i] = num;
+                break;
+            } else {
+                continue;
+            }
+        }
+        setPin(tempCode);
+    }
+
+    const onPressCancel = () => {
+        let tempCode = [...pin];
+        for (var i = tempCode.length - 1; i >= 0; i--) {
+            if (tempCode[i] !== '') {
+                tempCode[i] = '';
+                break;
+            } else {
+                continue;
+            }
+        }
+        setPin(tempCode);
+    }
 
     useEffect(() => {
         if (route.params?.prevPage){
@@ -22,46 +68,117 @@ const PinPage = () => {
         }
     }, [route.params])
 
+    useEffect(() => {
+        if (!pin.includes('')) {
+            handleSubmitPassCode();
+        }
+    }, [pin]);
+
+    const handleSubmitPassCode = () => {
+        const passCodeStringVal = pin.join('')
+        handleClearPassCode();
+        alert(passCodeStringVal)
+    }
+
+    const handleClearPassCode = () => {
+        setPin(['', '', '', ''])
+    }
+
     return (
         <MainContainer>
-            <View style={{alignItems: 'center'}}>
+            <View style={{flex: 1, alignItems: 'center'}}>
                 <View style={{width: '50%'}}>
                     <Text style={[theme.text.subtitle2, {textAlign: 'center'}]}>
                         Please input PIN {'\n'} (User id : 123)
                     </Text>
-                    <TextInput
+                    <View style={styles.pinContainer}>
+                        {pin.map((p) => {
+                            let style = p != '' ? styles.pinInput2 : styles.pinInput1;
+                            return <View style={style}></View>
+                        })}
+                    </View>
+                    {/* <TextInput
                         secureTextEntry
                         style={styles.pinInput}
                         value={pin}
                         onChangeText={setPin}
                         keyboardType='numeric'
                         maxLength={6}
+                    /> */}
+                </View>
+            </View>
+            <View style={{flex: 3}}>
+                <View style={styles.inputContainer}>
+                    <FlatList data={arr}
+                        renderItem={renderItem}
+                        numColumns={3}
+                        columnWrapperStyle={{justifyContent: 'space-around'}}
                     />
                 </View>
             </View>
-            <FormButton
-                onClick={() => {
-                    navigation.reset({
-                        index: 0,
-                        routes: [{
-                            name: pinParam.prevPage,
-                            params: {message : 'OK'}
-                        }]
-                    });
-                }}
-                label={'Submit'}
-            ></FormButton>
+            <View>
+               <TouchableOpacity onPress={() => onPressCancel()}>
+                    <Text style={[theme.text.subtitle2, {textAlign: 'center', marginBottom: 30}]}>Cancel</Text>
+               </TouchableOpacity>
+            </View>
+            <View style={{flex: 1}}>
+                <FormButton
+                    onClick={() => {
+                        navigation.reset({
+                            index: 0,
+                            routes: [{
+                                name: pinParam.prevPage,
+                                params: {message : 'OK'}
+                            }]
+                        });
+                    }}
+                    label={'Submit'}
+                ></FormButton>
+            </View>
         </MainContainer>
     )
 }
 
 const styling = (theme) => StyleSheet.create({
-    pinInput : {
-        borderBottomColor: theme.colors.foreground,
-        borderBottomWidth: 1,
-        marginVertical: theme.spacing.l,
-        fontSize: 32,
-        textAlign: 'center',
+    pinInput1 : {
+        width: 13,
+        height: 13,
+        borderRadius: 13,
+        borderWidth: 2,
+        borderColor: theme.colors.orange
+    },
+    pinInput2 : {
+        width: 13,
+        height: 13,
+        borderRadius: 13,
+        backgroundColor: theme.colors.orange
+    },
+    pinContainer:{
+        marginTop: 40,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    inputView : {
+        width: 64,
+        height: 64,
+        margin: theme.spacing.s
+    },
+    inputOval : {
+        borderColor: theme.colors.orange,
+        borderWidth: 2,
+        borderRadius: 32,
+        overflow: 'hidden',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 12
+    },
+    inputContainer : {
+        alignItems : 'center',
+    },
+    textNumber : {
+        color: theme.colors.orange,
+        fontFamily: 'Poppins-Bold'
     }
 })
 
