818e55f72982c0028c8c1de10aab63acfe73ae48
Ziana Mega Devi <zianamegadevi@gmail.com>
2022-09-07 16:53:25 +0700

    building app

--- app.config.js
+++ app.config.js
@@ -1,7 +1,14 @@
-export default {
-    name : 'my-app',
-    version : '1.0.0',
-    extra : {
-        baseUrl: process.env.BASE_URL
+const IS_DEV = process.env.APP_VARIANT === 'development'
+export default ({config}) => {
+    return {
+        ...config,
+        name : 'WMB',
+        version : process.env.CURR_VERSION || '1.0.0',
+        extra : {
+            baseUrl: process.env.BASE_URL
+        },
+        android: {
+            package: IS_DEV ? 'com.enigma.dev.myapp' : 'com.enigma.myapp'
+        }
     }
 }
